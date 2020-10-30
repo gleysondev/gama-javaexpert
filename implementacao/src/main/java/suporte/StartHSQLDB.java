aa351b774c9a695c28adb7ec532284fe59d1967f
Gleyson Sampaio <gleysondev@gmail.com>
2020-10-29 22:25:39 -0300

    hsqldb server

---
+++ implementacao/src/main/java/suporte/StartHSQLDB.java
@@ -0,0 +1,25 @@
+package suporte;
+
+import org.hsqldb.util.DatabaseManagerSwing;
+
+public class StartHSQLDB {
+	static String FILE_URL = "file:/gama/database/gamadb";
+
+	public static void main(String[] args) {
+		// local();
+		server();
+	}
+
+	public static void local() {
+		String[] dbArgs = { "--user", "sa", "--password", "", "--url", "jdbc:hsqldb:" + FILE_URL };
+		DatabaseManagerSwing.main(dbArgs);
+	}
+
+	public static void server() {
+		final String[] dbArgs = { "--database.0", FILE_URL, "--dbname.0", "gamadb", "--port", "5454" };
+		org.hsqldb.server.Server.main(dbArgs);
+		// final String[] serveArgs = { "--user", "sa", "--password", "", "--url",
+		// "jdbc:hsqldb:hsql://localhost:5454/gamadb"};
+		// DatabaseManagerSwing.main(serveArgs);
+	}
+}
