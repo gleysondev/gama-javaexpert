4e24bf9d5640317a56ef4d83fbbfa1055d9d7d52
Gleyson Sampaio <gleysondev@gmail.com>
2020-10-30 18:59:52 -0300

    alterando o banco de dados para o MySql

--- implementacao/src/main/java/suporte/StartHSQLDB.java
+++ implementacao/src/main/java/suporte/StartHSQLDB.java
@@ -1,6 +1,6 @@
 package suporte;
 
-import org.hsqldb.util.DatabaseManagerSwing;
+///import org.hsqldb.util.DatabaseManagerSwing;
 
 public class StartHSQLDB {
 	static String FILE_URL = "file:/gama/database/gamadb";
@@ -12,12 +12,12 @@ public class StartHSQLDB {
 
 	public static void local() {
 		String[] dbArgs = { "--user", "sa", "--password", "", "--url", "jdbc:hsqldb:" + FILE_URL };
-		DatabaseManagerSwing.main(dbArgs);
+		//DatabaseManagerSwing.main(dbArgs);
 	}
 
 	public static void server() {
 		final String[] dbArgs = { "--database.0", FILE_URL, "--dbname.0", "gamadb", "--port", "5454" };
-		org.hsqldb.server.Server.main(dbArgs);
+		//org.hsqldb.server.Server.main(dbArgs);
 		// final String[] serveArgs = { "--user", "sa", "--password", "", "--url",
 		// "jdbc:hsqldb:hsql://localhost:5454/gamadb"};
 		// DatabaseManagerSwing.main(serveArgs);
