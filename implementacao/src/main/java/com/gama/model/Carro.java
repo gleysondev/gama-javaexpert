7c719f5ff2636e01dcc96fea8d42eb029212239d
Gleyson Sampaio <gleysondev@gmail.com>
2020-10-29 21:13:09 -0300

    criando a nossa entidade carro

---
+++ implementacao/src/main/java/com/gama/model/Carro.java
@@ -0,0 +1,64 @@
+package com.gama.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_carro")
+public class Carro {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String marca;
+	
+	private String modelo;
+
+	private String cor;
+	
+	@Column(name="capacidade_tanque")
+	private Integer capacidadeTanque;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+	public Integer getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(Integer capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+	
+}
