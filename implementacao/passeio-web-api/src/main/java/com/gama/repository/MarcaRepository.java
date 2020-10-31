c9fa2df68bd0a6c4196039a8641524c297daf6fe
Gleyson Sampaio <gleysondev@gmail.com>
2020-10-31 16:23:06 -0300

    consulta avancadas com CrudRepostiory

--- implementacao/passeio-web-api/src/main/java/com/gama/repository/MarcaRepository.java
+++ implementacao/passeio-web-api/src/main/java/com/gama/repository/MarcaRepository.java
@@ -1,9 +1,21 @@
 package com.gama.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.gama.model.Marca;
 
 public interface MarcaRepository extends CrudRepository<Marca, Integer> {
-
+	@Query("SELECT e FROM Marca e WHERE e.nome = :p1")
+	Marca buscarPorNome(@Param("p1") String nome);
+	
+	Marca findByNome(String nome);
+	
+	List<Marca> findByNomeContaining(String nome);
+	
+	//@Query("SELECT true FROM Marca e WHERE e.nome = :nome")
+	//boolean existeMarcaPorNome(String nome);
 }
--- implementacao/passeio-web-api/src/main/java/com/gama/start/PasseioStart.java
+++ implementacao/passeio-web-api/src/main/java/com/gama/start/PasseioStart.java
@@ -1,11 +1,14 @@
 package com.gama.start;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.gama.model.Carro;
 import com.gama.model.Marca;
+import com.gama.repository.CarroRepository;
 import com.gama.repository.MarcaRepository;
 
 
@@ -15,6 +18,9 @@ public class PasseioStart {
 	
 	@Autowired
 	private MarcaRepository marcaRepository;
+	
+	@Autowired
+	private CarroRepository carroRepository;
 	
 	@Value("${meunome}")
 	private String nome;
@@ -30,19 +36,37 @@ public class PasseioStart {
 		gerenciarMarcas();
 	}
 	private void gerenciarMarcas() {
-	
-		Marca marca = new Marca();
-		marca.setNome("HYUNDAY");
+		//garantir que só haja uma marca HYUNDAY no banco de dados
+		String nome= "HYUNDAY";
 		
-		marcaRepository.save(marca);
+		//boolean existe = marcaRepository.existeMarcaPorNome(nome);
+		List<Marca> marcas = marcaRepository.findByNomeContaining(nome);
+		
+		Marca marca =marcaRepository.findByNome(nome);
+		if(marca==null) {
+			marca= new Marca();
+			marca.setNome(nome);
+			
+			marcaRepository.save(marca);
+		}
+		
+		
+		
 	}
 	private void gerenciarCarros() {
+		
+		//cadastrar 1 carro com a PLAXA ABC-1234
+		
+		
 		Carro carro = new Carro();
 		carro.setCapacidadeTanque(100);
 		carro.setCor("PRETA");
+		
+		//localizar a marca e incluir no novo carro
+		
 		//carro.setMarca(marca);
 		carro.setModelo("HB-20");
 		
-		//CarroRepository carroRepository = ctx.getBean();
+		
 	}
 }
