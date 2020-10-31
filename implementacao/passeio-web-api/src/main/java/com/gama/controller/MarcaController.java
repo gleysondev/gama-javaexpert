4c0a05855e16015719538604b4e34a670968152d
Gleyson Sampaio <gleysondev@gmail.com>
2020-10-31 17:39:20 -0300

    introdução de disponibilizar nossa aplicaca via API

--- implementacao/passeio-web-api/src/main/java/com/gama/controller/MarcaController.java
+++ implementacao/passeio-web-api/src/main/java/com/gama/controller/MarcaController.java
@@ -1,14 +1,57 @@
 package com.gama.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gama.model.Marca;
+import com.gama.repository.MarcaRepository;
+
+
 @RestController
-//@RequestMapping("/marcas")
+@RequestMapping("/marcas")
 public class MarcaController {
-	@GetMapping("/marcas")
-	public String hello() {
-		return "Ola Marcas";
+	@Autowired
+	private MarcaRepository repository;
+	
+	@GetMapping
+	public Iterable<Marca> listar() {
+		return repository.findAll();
 	}
+	
+	@GetMapping("/filter")
+	public Iterable<Marca> listar(@RequestParam("nome") String nome) {
+		return repository.findByNomeContaining(nome);
+	}
+	
+	@GetMapping("/{id}")
+	public Marca buscar(@PathVariable(value="id") Integer id) {
+		Optional<Marca> opt = repository.findById(id);
+		return opt.get() ;
+	}
+	
+	@PostMapping
+	public void save(@RequestBody Marca marca) {
+		repository.save(marca);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable(value="id") Integer id) {
+		repository.deleteById(id);
+	}
+	
+	@PutMapping
+	public void update(Marca marca) {
+		repository.save(marca);
+	}
+	
 }
